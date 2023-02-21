cffd0c3fae0cdd472ab24540978a5e34cd07ae4b
Denis <95048755+rowhe@users.noreply.github.com>
2023-02-21 20:25:44 +0300

    yandex_vpc_network yandex_vpc_subnets yandex_vpc_route_table yandex_compute_instance added

--- 15.1/infrastructure/terraform/main.tf
+++ 15.1/infrastructure/terraform/main.tf
@@ -5,41 +5,113 @@ terraform {
   required_version = ">= 0.13"
 }
 
-provider "yandex" {
-    token               = "var.yc_token"
-    cloud_id            = "var.yc_cloud_id"
-    folder_id           = "var.yc_folder_id"
-    zone                = "var.yc_region"
-}
-
 resource "yandex_vpc_network" "diploma" {
-    name                = "diploma-net"
+  name = "diploma-net"
 }
 
-
-
-// Create SA
-resource "yandex_iam_service_account" "sa" {
-  folder_id = "${var.yc_folder_id}"
-  name      = "tf-test-sa"
+resource "yandex_vpc_subnet" "public" {
+  name = "public-subnet"
+  v4_cidr_blocks = ["192.168.10.0/24"]
+  zone = "ru-central1-a"
+  network_id = "${yandex_vpc_network.diploma.id}"
+  description = "public subnet for diploma purposes"
+  route_table_id = "${yandex_vpc_route_table.diploma-private.id}"
 }
 
-// Grant permissions
-resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
-  folder_id = "${var.yc_folder_id}"
-  role      = "storage.editor"
-  member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
+resource "yandex_vpc_subnet" "private" {
+  name = "private-subnet"
+  v4_cidr_blocks = ["192.168.20.0/24"]
+  zone = "ru-central1-a"
+  network_id = "${yandex_vpc_network.diploma.id}"
+  description = "private subnet for diploma purposes"
+  route_table_id = "${yandex_vpc_route_table.diploma-private.id}"
+  }
+
+resource "yandex_vpc_route_table" "diploma-private" {
+  network_id = "${yandex_vpc_network.diploma.id}"
+    static_route {
+      destination_prefix = "0.0.0.0/0"
+      next_hop_address = "192.168.10.254"
+    }
 }
 
-// Create Static Access Keys
-resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
-  service_account_id = yandex_iam_service_account.sa.id
-  description        = "static access key for object storage"
+resource "yandex_compute_instance" "nat" {
+  name = "nat-instance"
+  zone = "ru-central1-a"
+  allow_stopping_for_update = true
+  scheduling_policy {
+    preemptible = true
+  }
+
+  resources {
+    cores = 2
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd80mrhj8fl2oe87o4e1"
+    }
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.public.id}"
+    ip_address = "192.168.10.254"
+    nat = true
+    }
+
+   metadata = {
+  ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
 }
 
-// Use keys to create bucket
-resource "yandex_storage_bucket" "stage732-1235gwosn" {
-  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
-  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
-  bucket = "bigbucket"
+resource "yandex_compute_instance" "pub-host1" {
+  name = "pub-host1-instance"
+  zone = "ru-central1-a"
+  allow_stopping_for_update = true
+  scheduling_policy {
+    preemptible = true
+  }
+  resources {
+    cores = 2
+    memory = 4
+  }
+  boot_disk {
+    initialize_params {
+      image_id = "fd864gbboths76r8gm5f"
+    }
+  }
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.public.id}"
+    ip_address = "192.168.10.10"
+    nat = true
+  }
+  metadata = {
+  ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+resource "yandex_compute_instance" "priv-host1" {
+  name = "priv-host1-instance"
+  zone = "ru-central1-a"
+  allow_stopping_for_update = true
+  scheduling_policy {
+    preemptible = true
+  }
+  resources {
+    cores = 2
+    memory = 4
+  }
+  boot_disk {
+    initialize_params {
+      image_id = "fd864gbboths76r8gm5f"
+    }
+  }
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.private.id}"
+    ip_address = "192.168.20.20"
+    }
+  metadata = {
+  ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
 }
